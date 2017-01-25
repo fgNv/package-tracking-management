12b750f01d72996214a41053ba726bcb1941a94b
fgUnderlost <felipe.underlost@gmail.com>
2017-01-25 11:10:11 -0200

    project structure

---
+++ package-tracking-management-view/src/main.js
@@ -0,0 +1,14 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+require('semantic-ui-css/semantic.css')
+require('semantic-ui-css/semantic.js')
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
