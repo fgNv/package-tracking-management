658f4931dca65e2e3560680d7afe138c091522e0
fgUnderlost <felipe.underlost@gmail.com>
2017-02-05 10:17:26 -0200

    removing attempt to workaround fsi json provider execution error

--- PackageTrackingManagement.Domain/Sentences.fs
+++ PackageTrackingManagement.Domain/Sentences.fs
@@ -2,8 +2,8 @@
 
 open FSharp.Data
 
-type PtBrValidationSentences = JsonProvider<"../PackageTrackingManagement.Domain/validation.pt-BR.json", ResolutionFolder = __SOURCE_DIRECTORY__>
-type PtBrErrorSentences = JsonProvider<"../PackageTrackingManagement.Domain/error.pt-BR.json", ResolutionFolder = __SOURCE_DIRECTORY__>
+type PtBrValidationSentences = JsonProvider<"../PackageTrackingManagement.Domain/validation.pt-BR.json">
+type PtBrErrorSentences = JsonProvider<"../PackageTrackingManagement.Domain/error.pt-BR.json">
 
 let Validation = 
     PtBrValidationSentences.Load(__SOURCE_DIRECTORY__ + "/validation.pt-BR.json")
